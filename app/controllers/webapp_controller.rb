cea012cd15e718beb45e3bb8ee5a3ee7ac62f21e
Patrick Negri <patrick@iugu.com.br>
2012-10-14 22:26:28 -0300

    [ ! ] Created Default Views and Routes

--- app/controllers/webapp_controller.rb
+++ app/controllers/webapp_controller.rb
@@ -1,7 +1,11 @@
 class WebappController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index]
 
   def entry_point
     render :layout => false
+  end
+
+  def index
+    render 'webapp/welcome' unless current_user
   end
 end
